bd33a3182910140512ec767ed7b04cc4fde61e1b
Koussay Rachidi <koussayrachidi2@gmail.com>
2023-04-04 10:14:02 +0100

    first commit

---
+++ src/Entity/Article.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type:"string", length:255)]
+    #[Assert\Length(
+    min : 5,
+    max : 50,
+    minMessage : "Le nom d'un article doit comporter au moins {{ limit }} caractères",
+    maxMessage : "Le nom d'un article doit comporter au plus {{ limit }} caractères"
+    )]
+    private ?string $nom = null;
+
+    #[ORM\Column(type:"decimal", precision:10, scale:0)]
+    #[Assert\NotEqualTo(
+    value : 0,
+    message : "Le prix d'un article ne doit pas être égal à 0 ",
+    )]
+    private ?string $prix = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
